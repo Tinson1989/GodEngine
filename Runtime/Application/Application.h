d51c35af27c0a1678ab139e37f49df50edc49b0b
陈田生 <chentiansheng@sandboxol.com>
2022-07-18 13:27:45 +0800

    添加基础基类

---
+++ Runtime/Application/Application.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "IApplication.h"
+#include "Memory/MemoryManager.h"
+#include "Runtime/RHI/GraphicsMgr.h"
+// #include "Runtime/Core/Time/TimeMgr.h"
+// #include "Runtime/Core/Input/InputMgr.h"
+// #include "Runtime/Core/Object/World.h"
+// #include "Runtime/Core/UI/Director.h"
+// #include "Runtime/Core/Font/FontMgr.h"
+// #include "Runtime/Core/ScriptEngine/ScriptEngine.h"
+
+
+namespace GodEngine {
+	class Application : public IApplication {
+
+	public:
+		virtual int		Initialize() noexcept;
+		virtual void	Tick() noexcept;
+		virtual void	Render() noexcept;
+		virtual void	Run() noexcept;
+		virtual void	Finalize() noexcept;
+		virtual void	Quit() noexcept;
+		virtual bool	IsQuit() noexcept;
+
+	public:
+		GraphicsManager*	mGraphicsManager;
+		// TimeMgr*			mTimeMgr;
+		// InputMgr*			mInputManager;
+		// FontMgr*			mFontMgr;
+		// Director*			mDirector;
+		// World*				mWorld;
+		// IScriptEngine*		mVM;
+
+	private:
+		bool mQuit;
+
+	};
+
+	extern Application* GApp;
+}
---
+++ Runtime/Application/IApplication.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "Runtime/Logging/Assert.h"
+
+namespace GodEngine {
+
+#define CHECK_APPLICATION_INIT(result)	if(result!=0){ exit(result);}
+	
+	class IApplication {
+	public:
+		virtual int Initialize() noexcept = 0;
+		virtual void Run() noexcept = 0;
+		virtual void Tick() noexcept = 0;
+		virtual void Render() noexcept = 0;
+		virtual void Finalize() noexcept = 0;
+		virtual void Quit() noexcept = 0;
+		virtual bool IsQuit() noexcept = 0;
+	};
+}
